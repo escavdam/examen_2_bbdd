ec7962661762a36441896b197801e146f05010e6
PC156\Alumno <pacomateostomas@gmail.com>
2024-03-14 14:10:58 +0100

    updated index.js

--- index.js
+++ index.js
@@ -1,9 +1,83 @@
 const express = require('express')
+const moment = require('moment')
 const app = express()
 
-app.get("/saludo", (req, res) => {
-    res.send("hola")
+app.get('/saludo', (req, res) => {
+    const { nombre } = req.query
+    res.send(`holla ${nombre}`)
 })
 
-app.listen(3000, () => console.log('Server open in port 3000')) 
+app.get('/random', (req, res) => {
+    const randomNumber = Math.floor(Math.random()*1)
+    let boolean
+    if(randomNumber > 0.5){
+        boolean = true
+    }
+    else{
+        boolean = false
+    }
+    const randomRange100 = Math.floor(Math.random()*100)
+    const randomRange255 = Math.floor(Math.random()*255)
+    const miobjeto = {
+        randomNumber,
+        boolean,
+        randomRange100,
+        randomRange255
+    }
+    res.json(miobjeto)
+})
 
+app.get('/fecha', (req, res) =>{
+    const tiempo = moment().format('MMMM Do YYYY, h:mm:ss a')
+    res.send(tiempo)
+})
+
+app.get('/1337', (req, res) => {
+    const mensaje = req.query.mensaje
+    const leet = mensaje.split("")
+
+    for(let i = 0; i <= leet.length; i++ ) {
+        console.log(leet[i])
+        if (leet[i] === "e") {
+            leet[i] = "3"
+        }
+        if (leet[i] === "a") {
+            leet[i] = "4"
+        }
+        if (leet[i] === "o") {
+            leet[i] = "0"
+        }
+        if (leet[i] === "s") {
+            leet[i] = "5"
+        }
+        if (leet[i] === "t") {
+            leet[i] = "7"
+        }
+        if (leet[i] === "l") {
+            leet[i] = "1"
+        }
+        if (leet[i] === "z") {
+            leet[i] = "2"
+        }
+        if (leet[i] === "b") {
+            leet[i] = "8"
+        }
+        if (leet[i] === "g") {
+            leet[i] = "9"
+        }
+        if (leet[i] === "q") {
+            leet[i] = "9"
+        }
+    }
+
+    const reformed = leet.join("")
+
+    res.send(reformed)
+})
+
+app.get ('/tokipona', (req, res) =>{
+    
+}
+)
+
+app.listen(3000, () => console.log('Server open in port 3000')) 
